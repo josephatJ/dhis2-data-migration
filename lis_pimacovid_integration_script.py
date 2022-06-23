a99c46877ea5cd4c8fa6ca15aa78622169f26774
josephatJ <josephatjulius24@gmail.com>
2022-06-23 17:38:35 +0300

    Add support to capture variables from .env file

--- lis_pimacovid_integration_script.py
+++ lis_pimacovid_integration_script.py
@@ -1,8 +1,11 @@
 
-# Josephat Mwakyusa, August 06 2021
+# Josephat Mwakyusa & UDSM DHIS2 LAB, June 23 2021
 
 import asyncio
 import json
+import os
+# sudo pip3 install python-dotenv
+from dotenv import dotenv_values
 from requests.auth import HTTPBasicAuth
 from utilities.get_dhis2_data  import DHIS2PersonData
 
@@ -11,10 +14,12 @@ headers = {
 }
 
 
+
 async def main():
-    username = 'lispimacovidintegration'
-    password = 'Dhis@2022'
-    url = 'https://covid19-admin.moh.go.tz'
+    configs = dotenv_values(".env")
+    username = configs.get('USERNAME')
+    password = configs.get('PASSWORD')
+    url = configs.get('URL')
     dhis2PersonData = DHIS2PersonData(username,password,url)
     dhis2PersonData.get_client_details()
 
